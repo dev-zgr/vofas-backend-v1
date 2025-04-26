a198b661aee635d510c711e8c5bbdf512885ccad
dev-zgr <ozgurkamalipersonal@gmail.com>
2025-04-26 18:12:00 +0300

    Initial commit

---
+++ src/main/java/org/example/vofasbackendv1/VofasBackendV1Application.java
@@ -0,0 +1,13 @@
+package org.example.vofasbackendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VofasBackendV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VofasBackendV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/vofasbackendv1/VofasBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vofasbackendv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VofasBackendV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
