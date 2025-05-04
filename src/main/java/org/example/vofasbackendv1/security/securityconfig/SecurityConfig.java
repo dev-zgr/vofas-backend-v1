aa28fb7debe69b8dbe2f4026ef1f214c8d45fc86
dev-zgr <ozgurkamalipersonal@gmail.com>
2025-05-04 18:53:10 +0300

    Security configuration updated.

--- src/main/java/org/example/vofasbackendv1/security/securityconfig/SecurityConfig.java
+++ src/main/java/org/example/vofasbackendv1/security/securityconfig/SecurityConfig.java
@@ -1,15 +1,13 @@
 package org.example.vofasbackendv1.security.securityconfig;
 import org.example.vofasbackendv1.security.filter.JwtAuthenticationFilter;
-import org.example.vofasbackendv1.servicelayer.implementations.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,11 +31,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(e -> e.authenticationEntryPoint(entryPoint))
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/vofas/api/v1/user/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/vofas/api/v1/static-qr/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/vofas/api/v1/static-qr/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/vofas/api/v1/static-qr/**").hasAnyAuthority("ADMIN", "COMPANY_REPRESENTATIVE")
                         .requestMatchers("/vofas/api/v1/**").permitAll()
                         .requestMatchers("/swagger-ui").permitAll()
                         .anyRequest().authenticated()
